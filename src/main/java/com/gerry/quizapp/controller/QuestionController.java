51a49cf20730145be4b3e16c35fafb0a41920d2b
OG-Oluoch <makopiyo45@gmail.com>
2025-02-08 21:32:20 +0300

    Initial commit

---
+++ src/main/java/com/gerry/quizapp/controller/QuestionController.java
@@ -0,0 +1,33 @@
+package com.gerry.quizapp.controller;
+
+import com.gerry.quizapp.model.Question;
+import com.gerry.quizapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("questions")
+public class QuestionController {
+
+    @Autowired
+    QuestionService questionService;
+
+    @GetMapping("allQuestions")
+    public List<Question> getAllQuestions(){
+
+        return questionService.getAllQuestions();
+    }
+
+    @GetMapping("category/{category}")
+    public List<Question> getQuestionCategory (@PathVariable String category){
+
+
+return questionService.getQuestionByCategory(category);
+
+    }
+}
---
+++ src/main/java/com/gerry/quizapp/service/QuestionService.java
@@ -0,0 +1,25 @@
+package com.gerry.quizapp.service;
+
+import com.gerry.quizapp.dao.QuestionDao;
+import com.gerry.quizapp.model.Question;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    QuestionDao questionDao;
+
+    public List<Question> getAllQuestions() {
+
+       return questionDao.findAll();
+    }
+
+    public List<Question> getQuestionByCategory(String category) {
+
+        return questionDao.findByCategory(category);
+    }
+}
