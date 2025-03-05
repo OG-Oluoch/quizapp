58a5b1108253f91b56542d5f5a2d3e1b9125b9b4
OG-Oluoch <makopiyo45@gmail.com>
2025-03-05 11:20:54 +0300

    fixed lombok error

--- src/main/java/com/gerry/quizapp/model/Quiz.java
+++ src/main/java/com/gerry/quizapp/model/Quiz.java
@@ -1,9 +1,6 @@
 package com.gerry.quizapp.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.util.List;
@@ -16,5 +13,23 @@ public class Quiz {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String title;
+
+    @ManyToMany
     private List<Question> questions;
+
+    public List<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<Question> questions) {
+        this.questions = questions;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
